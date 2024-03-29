6506053415cf5a2d391f1e4d800438d11daa735f
nicoleUY <nicole_uy@dlsu.edu.ph>
2024-03-30 01:54:07 +0800

    added prompts, doesnt rlly store values I tink

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,7 +1,18 @@
 ;assembly part using x86-64
 
 section .data
-msg db "Hello World", 13, 10, 0		;13 10 is the \n
+prompt1 db "Enter scalar variable n: ", 0		;13 10 is the \n
+prompt2 db "Enter values of vector A (single precision): ", 0
+prompt3 db "Enter values of vector B (single precision): ", 0
+
+n dq 0
+var1 dq 0.0
+var2 dq 0.0
+
+scanInt db "%d",0
+scanFloat db "%f",0
+
+msg db "Da numbers are %d, %f, %f", 13,10,0
 
 section .text
 bits 64								;to tell 64 bits
@@ -9,14 +20,39 @@ default rel							;to support address relocation and relativity
 
 									;above is standard part of template
 
-global asmhello						;so that c can regognize, equivalent of extern
-extern printf
+global get_input				;so that c can regognize, equivalent of extern
+extern printf, scanf
 
-asmhello:
+get_input:
 	sub rsp, 8*5					;caller: if the assembly is caller we allocate stack pointer stack frame with 40 bytes
-	lea rcx, [msg]
+	lea rcx, [prompt1]				;Enter scalar variable n: 
 	call printf
+
+	lea rdx, [n]
+	mov rcx, scanInt
+	call scanf
+
+	lea rcx, [prompt2]				;Enter values of vector A (single precision): 
+	call printf
+
+	lea rdx, [var1]
+	mov rcx, scanFloat
+	call scanf
+
+	lea rcx, [prompt3]				;Enter values of vector B (single precision): 
+	call printf
+	
+	lea rdx, [var2]
+	mov rcx, scanFloat
+	call scanf
+
+							;DIS NEEDS TO BE EDITED PA, IT SOWS GARBAGE VALS LANG
+	lea rcx, [msg]					;"Da numbers are %d, %f, %f", 13,10,0
+	call printf
+
 	add rsp, 8*5					;adjust back the pointer
+
+
 	ret
 
 
--- main.c
+++ main.c
@@ -6,8 +6,11 @@ extern void asmhello();
 
 extern int summ(int a1, int a2, int a3, int a4, int a5, int a6, int a7);
 
+extern float get_input();
+
 int main() {
-	asmhello();
+
+	//asmhello();
 	/*
 	int a1, a2, a3, a4, a5, a6, a7, x;
 	a1 = 1;
@@ -19,5 +22,9 @@ int main() {
 	a7 = 100;
 	x = summ(a1, a2, a3, a4, a5, a6, a7);
 	printf("%d + %d + %d + %d + %d + %d + %d = %d \n", a1, a2, a3, a4, a5, a6, a7, x);*/
+
+	//ACTUAL PROJECT
+	get_input();
+
 	return 0;
 }
